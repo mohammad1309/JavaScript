68551711f66d05db2cb31de6aabfc35542b36851
mohammad1309 <ayaansheikh.r2b@gmail.com>
2025-09-26 18:31:06 +0000

    variables and data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// cant use alert becs we are using node js not browser
+
+console.log(3+3)
+console.log("ayaan")
+
+
+/*
+some primitive datatypes
+
+numbers -> 2 to the power 53
+bigint
+string -> ""
+boolean -> true/false
+null -> standalone value
+undefined -> 
+symbol -> unique
+*/
+
+console.log(typeof null) // object 
+console.log(typeof numbers)
+console.log(typeof undefined) // undefined
---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 18221
+let name = "Ayaan"
+accountCity = "Ujjain"
+let accountState;
+
+console.log(accountId)
+console.log("name")
+console.log(accountCity)
+
+// can use var but prefer not to use it becs of scope problem
+
+console.table([accountId, "name", accountCity, accountState])
