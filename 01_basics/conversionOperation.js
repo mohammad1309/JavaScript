0b69abf2b0c52329fd44d759c1bfba381c3a7640
mohammad1309 <ayaansheikh.r2b@gmail.com>
2025-09-27 09:53:00 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,20 +1,33 @@
 let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 let isLoggedIn = true
-console.log(typeof isLoggedIn)
+// console.log(typeof isLoggedIn)
 let valueIsLoggedIn = Number(isLoggedIn)
-console.log(valueIsLoggedIn)
+// console.log(valueIsLoggedIn)
 
 let boolIsLoggedIn = Boolean(valueIsLoggedIn)
-console.log(boolIsLoggedIn)
+// console.log(boolIsLoggedIn)
 
 // we can even use 'String' to convert any variable to string
 // 1 -> true , 0 -> false
 // "" -> false  ,  "Ayaan" -> true
+
+// ********** OPERATIONS **********
+
+let value = 4
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) // 2 race to the power 3
+// console.log(2/2)
+// console.log(2%2)
