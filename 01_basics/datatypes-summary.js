7b1fa195b575901d44b3dc2fbce04bdd673ae40a
mohammad1309 <ayaansheikh.r2b@gmail.com>
2025-11-26 05:18:50 +0000

    datatypes summ.

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,30 +1,30 @@
-// primitive datatypes
-/* 7 types :
-number, string, bigInt, null, undefined, symbol, boolean 
-*/
+// // primitive datatypes
+// /* 7 types :
+// number, string, bigInt, null, undefined, symbol, boolean 
+// */ 
 
-let id = Symbol('123')
-let id2 = Symbol('123')
+// let id = Symbol('123')
+// let id2 = Symbol('123')
 
-console.log(id === id2)
+// console.log(id === id2)
 
-// non-primitive datatypes
-// array, objects, functions
+// // non-primitive datatypes
+// // array, objects, functions
 
-let names = ["ayaan", "maham", "alwaaz"]
+// let names = ["ayaan", "maham", "alwaaz"]
 
-let myObj = {
-    name: "ayaan",
-    age: 21,
-}
+// let myObj = {
+//     name: "ayaan",
+//     age: 21,
+// }
 
-const myFunction = function(){
-    console.log("Hello World")
-}
+// const myFunction = function(){
+//     console.log("Hello World")
+// }
 
-console.log(names)
-console.log(myObj)
-console.log(myFunction) // ???? will learn about this
+// console.log(names)
+// console.log(myObj)
+// console.log(myFunction) // ???? will learn about this
 
 // memory
 // stack (primitive) , heap (non-primitive)
