34a2275bb1bb092052acb93cd9f50a081f7a9d43
mohammad1309 <ayaansheikh.r2b@gmail.com>
2025-11-26 05:37:22 +0000

    stringss

---
+++ 01_basics/strings.js
@@ -0,0 +1,7 @@
+const name = "Ayaan"
+// another way -> const gameName = new String('Ayaan')
+const repoCount = 5
+// console.log(name + repoCount + "Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+let gameName = "Ayaan-is-bad"
+console.log(gameName.split('-')) ;
