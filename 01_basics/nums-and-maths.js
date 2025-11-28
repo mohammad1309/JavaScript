7be17a427024d4026d9213b49ef56488be674db0
mohammad1309 <ayaansheikh.r2b@gmail.com>
2025-11-28 07:08:51 +0000

    nums and maths

---
+++ 01_basics/nums-and-maths.js
@@ -0,0 +1,11 @@
+const balance = new Number(1000);
+
+// console.log(balance.toString());
+// console.log(typeof (balance));
+
+//---------------------Maths-------------------------//
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
