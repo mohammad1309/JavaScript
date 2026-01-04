e92181dfe8835935bd4960e779d8e5c0f366819c
mohammad1309 <ayaansheikh.r2b@gmail.com>
2026-01-04 13:01:13 +0000

    basics

---
+++ 03_control_flow/basics.js
@@ -0,0 +1,16 @@
+/* 
+only if else basics
+switch case basics
+
+operators such as:
+>
+<
+>=
+<=
+!=
+==
+===
+?? ( nullish coalescing operator) use to check null and undefined
+ternary operator too
+
+*/
