a9e606ebe0af1b2cae3dad34bbdaf2633b4b85cb
mohammad1309 <ayaansheikh.r2b@gmail.com>
2026-01-04 10:43:57 +0000

    arrow functions

---
+++ 02_basics/arrow.js
@@ -0,0 +1,11 @@
+// explicit
+const addTwo = (num1, num2) => {
+    return num1+num2
+}
+
+console.log(addTwo(4,7))
+
+// implicit
+
+const addTwoNum = (num1, num2) => (num1 + num2)
+console.log(addTwo(4,9))
