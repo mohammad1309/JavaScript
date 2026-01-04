36f108492ba88abc9ea914ea7ba313e3b4b8b865
mohammad1309 <ayaansheikh.r2b@gmail.com>
2026-01-04 10:52:30 +0000

    iife

---
+++ 02_basics/iife.js
@@ -0,0 +1,10 @@
+ // immediately invoked finction expression (iife)
+
+(function siuu(){
+    console.log("connected")
+}) ();
+
+// () ()
+// jo function immediately execute ho jaye
+// global scope variable kay pollution say bachnay kay liye
+
